12ce62969c4275fc128cd30e136c019b51123d70
Larisa Nazarenko <loranazarenko@users.noreply.github.com>
2020-05-31 23:27:00 +0300

    Fixes format

--- Query/queries.go
+++ Query/queries.go
@@ -17,7 +17,18 @@ func getManagers() {
 	db, err := sql.Open("mysql", "root:root@/employees")
 	checkErr(err)
 
-	rows, err := db.Query("SELECT CONCAT(' Name - ', m.first_name, ' ', m.last_name, ', Title - ', t.title, ', Departament - ', d.dept_name,', Salary - ', s.salary) manager FROM employees m JOIN dept_manager dm ON dm.emp_no = m.emp_no JOIN dept_emp de ON m.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no AND dm.dept_no = de.dept_no JOIN titles t ON m.emp_no = t.emp_no JOIN salaries s ON m.emp_no = s.emp_no WHERE dm.to_date = '9999-01-01' AND t.to_date = '9999-01-01' AND s.to_date = '9999-01-01' ORDER BY manager;")
+	query := `SELECT CONCAT(
+		' Name - ',m.first_name,' ',m.last_name,' ','Title - ',t.title,' ','Departament - ',d.dept_name,' ','Salary - ',s.salary
+		) manager 
+			FROM employees m JOIN 
+			dept_manager dm ON dm.emp_no = m.emp_no JOIN 
+			dept_emp de ON m.emp_no = de.emp_no JOIN 
+			departments d ON de.dept_no = d.dept_no AND dm.dept_no = de.dept_no JOIN 
+			titles t ON m.emp_no = t.emp_no JOIN 
+			salaries s ON m.emp_no = s.emp_no 
+			WHERE dm.to_date > NOW() AND t.to_date > NOW() AND s.to_date > NOW() 
+			ORDER BY manager;`
+	rows, err := db.Query(query)
 	checkErr(err)
 	employee := Employee{}
 	employees := []Employee{}
@@ -37,7 +48,21 @@ func getEmployees() {
 	db, err := sql.Open("mysql", "root:root@/employees")
 	checkErr(err)
 
-	rows, err := db.Query("SELECT CONCAT('Congratulation with job anniversary!!!  Departament - ',d.dept_name, ', Title - ',t.title, ', Name - ', e.first_name, '  ', e.last_name, ', Hire date - ', e.hire_date, ', Experience - ', DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), e.hire_date)),'%Y')+0) employee FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no AND e.emp_no = de.emp_no JOIN titles t ON e.emp_no = t.emp_no WHERE MONTH(now()) = MONTH(e.hire_date) ORDER BY employee LIMIT 1000;")
+	query := `
+	SELECT CONCAT(
+		'Congratulation with job anniversary!!!  
+		Departament - ',d.dept_name, ', Title - ',t.title, ', 
+		Name - ', e.first_name, '  ', e.last_name, ', Hire date - ', 
+		e.hire_date, ', Experience - ', DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), e.hire_date)),'%Y')+0
+		) employee 
+			FROM employees e JOIN 
+			dept_emp de ON e.emp_no = de.emp_no JOIN 
+			departments d ON de.dept_no = d.dept_no AND e.emp_no = de.emp_no JOIN 
+			titles t ON e.emp_no = t.emp_no 
+			WHERE (de.to_date > NOW() AND t.to_date > NOW() AND
+			MONTH(now()) = MONTH(e.hire_date))
+			ORDER BY employee;`
+	rows, err := db.Query(query)
 	checkErr(err)
 	employee := Employee{}
 	employees := []Employee{}
@@ -56,8 +81,17 @@ func getEmployees() {
 func getDepartmens() {
 	db, err := sql.Open("mysql", "root:root@/employees")
 	checkErr(err)
-
-	rows, err := db.Query("SELECT CONCAT(' Department - ',dept.dept_name, ', Employees_Number - ',COUNT(emp.emp_no), ', Dept_Salary - ', SUM(sal.salary)) from departments dept join dept_emp cdept on dept.dept_no = cdept.dept_no join employees emp on emp.emp_no = cdept.emp_no join salaries sal ON emp.emp_no = sal.emp_no WHERE cdept.to_date = '9999-01-01' AND sal.to_date = '9999-01-01' GROUP BY dept.dept_name ORDER BY dept.dept_name;")
+	query := `
+	SELECT CONCAT(
+		' Department - ',dept.dept_name, ', Employees_Number - ',COUNT(emp.emp_no), ', Dept_Salary - ', SUM(sal.salary))
+		FROM departments dept JOIN
+		 dept_emp cdept on dept.dept_no = cdept.dept_no JOIN
+		  employees emp on emp.emp_no = cdept.emp_no JOIN
+		   salaries sal ON emp.emp_no = sal.emp_no 
+		   WHERE cdept.to_date > NOW() AND sal.to_date > NOW() 
+		   GROUP BY dept.dept_name 
+		   ORDER BY dept.dept_name;`
+	rows, err := db.Query(query)
 	checkErr(err)
 	employee := Employee{}
 	employees := []Employee{}
